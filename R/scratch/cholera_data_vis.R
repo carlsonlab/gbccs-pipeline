21037c10a011167c8652a83a3c543dac57397ff9
colebrookson <cole.brookson@gmail.com>
2026-01-09 17:49:19 +0000

    need a bigger compute to do the transform lol

--- R/scratch/cholera_data_vis.R
+++ R/scratch/cholera_data_vis.R
@@ -17,11 +17,43 @@ table(surv_df$phantom)
 length(unique(surv_df$location_name))
 length(unique(surv_df$adm0))
 length(unique(surv_df$region))
-
+length(unique(surv_df$location_period_id))
 
 # Spatial exploration ----------------------------------------------------------
-pak::pkg_install("sf")
+library(ggplot2)
+library(sf)
+
 surv_shp <- readRDS(
     here::here("./data/cholera/raw/Public_surveillance_shapefiles.rds")
 )
-str(surv_shp)
+
+unique(surv_shp$location_period_id)
+sf::st_crs(surv_shp$geometry[[1]])
+
+# I think we can actually stitch this thing together into a map that has pretty
+# much all the information we could want? Going to try to do this by:
+#   1. creating a dataframe where the location names and the period ID are
+#      all together in a useable format
+#   2. then I'll bind the various sfg (polygons) together so I can use then as
+#      as a single object that shares geometry
+
+# this is slow...
+surv_full <- dplyr::left_join(
+    surv_shp,
+    surv_df |>
+        dplyr::select(location_period_id, region, adm0, adm1, adm2, adm3) |>
+        dplyr::mutate(location_period_id = as.character(location_period_id)),
+    by = "location_period_id"
+)
+
+class(surv_full)
+# there's no CRS defined so I'm going to set it go WGS84 Sinusoidal -- the no
+# CRS is actually because it's not a proper sfc object yet, but we're fixing
+# taht, so we can re-project wit
+surv_full$geometry <- sf::st_as_sfc(surv_full$geometry, crs = 3832)
+
+#
+surv_full <- sf::st_transform(surv_full, crs = 3832)
+
+ggplot() +
+    geom_sf(data = surv_shp$geometry[[1]], fill = "lightgrey")
