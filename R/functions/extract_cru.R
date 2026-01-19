1815f3ab45edf4065f4094922f860f596f83d540
Hailey Robertson <65562768+haileyrobertson@users.noreply.github.com>
2026-01-19 17:38:20 -0500

    switch to terra
    ncdf4 kept killing R because it took up too much RAM; terra is much nicer to work with anyway (don't have to transpose anything or make bricks)

--- R/functions/extract_cru.R
+++ R/functions/extract_cru.R
@@ -1,59 +1,58 @@
-# covs <- c("tmp", "tmn", "tmx", "pre", "pet")
-covs <- c("tmp")
-cru_data_dir <- here::here("data", "cru", "raw", "cru_ts_4.09")
-
-# TODO: Load polygon shapefiles to match Cholera data
-
 # First read in the CRU data for each variable of interest
 # Explicitly set each layer to the variable name
+# TODO: maybe use OS or similar to just grab all files in the data_dir? to avoid hard coding versions / years wihch might change
+# NOTE: Also tried this with ncdf4, but ran into RAM issues
 read_cru <- function(var, data_dir) {
   var_file <- paste0("cru_ts4.09.1901.2024.", var, ".dat.nc")
   var_path <- file.path(data_dir, var_file)
-  ncdf <- ncdf4::nc_open(var_path)
-  # must explicitly close for future bootstrpaping
-  on.exit(ncdf4::nc_close(ncdf))
 
-  # so to call will be like:
-  # cru$tmp$data
-  # cru$tmp$time
-  list(
-    # dim(cru$tmp$data) = 720 360 1488 [lon, lat, time]
-    data = ncdf4::ncvar_get(ncdf, var),
-    lon = ncdf4::ncvar_get(ncdf, "lon"),
-    lat = ncdf4::ncvar_get(ncdf, "lat"),
-    time = ncdf4::ncvar_get(ncdf, "time")
-  )
+  r <- terra::rast(var_path, subds = var)
+
+  # each layer = 1 momth
+  names(r) <- paste0(var, "_", seq_len(terra::nlyr(r)))
+  # gives back SpatRaster with dim [360, 720, 1488] = [lat, lon, time]
+  return(r)
 }
 
-cru <- lapply(covs, read_cru, data_dir = cru_data_dir)
+covs <- c("tmp", "tmn")
+# covs <- c("tmp", "tmn", "tmx", "pre", "pet")
+
+cru_data_dir <- here::here("data", "cru", "raw", "cru_ts_4.09")
+cru <- lapply(covs, read_cru, cru_data_dir)
+# name each spatraster with the variable
+# so you can call cru$tmp
 names(cru) <- covs
 
-# TODO: Convert time to dates maybe?
+# TODO: Load polygon shapefiles to match Cholera data
+# polys <- terra::vect("path/to/cholera.shp")
+# # PLACEHOLDER: Africa for testing
+# polys <- terra::ext(-30, 60, -37, 40) |> terra::as.polygons()
+# polys$ID <- 1
 
-# Validate dimensions since...
-# native CRU is x = lat, y = lon format
-# but that's different from native raster format (x = lon, y = lat)
-check_dims <- function(x) {
-  dim(x$data)[1] == length(x$lon)
+extract_var <- function(cru_raster, var, polygons) {
+  df <- terra::extract(cru_raster, polys, fun = mean, na.rm = TRUE)
+  dates <- terra::time(cru_raster)
+  long <- df %>%
+    tidyr::pivot_longer(
+      cols = -ID,
+      names_to = "layer",
+      values_to = var
+    ) %>%
+    dplyr::mutate(
+      layer = as.integer(gsub(paste0(var, "_"), "", layer)),
+      date = dates[layer]
+    ) %>%
+    dplyr::select(-layer)
+  return(long)
 }
-lapply(cru, check_dims)
 
-# Convert into raster native
-# Transposing the slice and flipping to lon-lat
-make_raster <- function(cru_slice) {
-  r <- raster::raster(t(cru_slice))
-  r <- raster::flip(r, direction = "y")
-  # raster::extent(r) <- c(-180, 180, -90, 90)
-}
+# TODO: Consider targets options
+tmp_mean <- extract_var(cru$tmp, "tmp", polys)
+tmn_mean <- extract_var(cru$tmn, "tmn", polys)
 
-# TODO: Make raster stack
+long_cru <- tmp_mean %>%
+  left_join(tmn_mean, by = c("ID", "date"))
 
-# TODO: Extract polygon means
-# Loop over time (in months?)
-# for each month (cru$var$data)[,,i]
-# make a raster for month i
-# then extract mean values for each polygon
-
-# TODO: Convert to tidy long
-# Split into month/year/var
-# Save as csv...
+out_path <- here::here("data", "cru", "clean")
+out_name <- paste0("cru_all_vars_", format(Sys.Date(), "%Y%m%d"), ".csv")
+write.csv(long_cru, file.path(out_path, out_name))
