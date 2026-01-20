e27e20fdcdf7ac3299925d563701f32e16cc522f
Hailey Robertson <65562768+haileyrobertson@users.noreply.github.com>
2026-01-20 12:49:31 -0500

    framing use of tokens
    the base url isnt working but that's a sep problem i think

--- R/functions/load_cru.R
+++ R/functions/load_cru.R
@@ -8,26 +8,69 @@
 #' @param file_format The file format of the data files. Default is "dat.nc" which is the NetCDF format.
 #' @return Nothing But downloads and unzips the files in the "data/cru/raw" directory.
 
-# TODO: Come back to this and make it handle access tokens instead
+# TODO: Come back to this and make it handle access tokens correctly (???)
 
-load_cru <- function(covs = c("tmp", "tmn", "tmx", "pre", "pet"),
-                     file_format = "dat.nc") {
-  base <- "https://data.ceda.ac.uk/badc/cru/data/cru_ts/cru_ts_4.09/data"
 
-  output_dir <- here::here("data", "cru", "raw", "cru_ts_4.09")
+# , "tmn", "tmx", "pre", "pet"
+load_cru <- function(covs = c("tmp"),
+                     file_format = "dat.nc",
+                     token = CEDA_TOKEN) {
+  # HELPER Generate new Archive Access Token from CEDA website
+  get_token <- function() {
+    user <- Sys.getenv("CEDA_USERNAME")
+    pass <- Sys.getenv("CEDA_PASSWORD")
+
+    resp <- httr2::request(
+      "https://services.ceda.ac.uk/api/token/create/"
+    ) |>
+      httr2::req_auth_basic(user, pass) |>
+      httr2::req_headers(Accept = "application/json") |>
+      httr2::req_method("POST") |>
+      httr2::req_perform()
+
+    httr2::resp_check_status(resp)
+
+    httr2::resp_body_json(resp)$access_token
+  }
+  base <- "https://dap.ceda.ac.uk/badc/cru/data/cru_ts/cru_ts_4.09/data/"
+  output_dir <- here::here("data", "cru", "raw", "test")
 
   for (var in covs) {
     file_name <- paste0("cru_ts4.09.1901.2024.", var, ".", file_format, ".gz")
-    url <- paste0(base, "/", var, "/", file_name)
+    url <- paste0(base, var, "/", file_name)
     dest <- file.path(output_dir, file_name)
 
     if (!file.exists(dest)) {
-      download.file(url, destfile = dest, mode = "wb")
-      # unzipping .gz but keeping them in the folder
-      # dest is the compressed file path, destname is the uncompressed file inside (the way splicing is INSANE in R???????)
-      R.utils::gunzip(dest, destname = substr(dest, 1, nchar(dest) - 3), remove = FALSE)
-    } else {
-      message(paste0("File", file_name, "already exists. Skipping download."))
+      req <- httr2::request(url) |>
+        httr2::req_auth_bearer_token(token) |>
+        httr2::req_method("GET")
+
+      resp <- httr2::req_perform(req)
+      if (httr2::resp_status(resp) == 401) {
+        token <- get_ceda_token()
+        resp <- httr2::request(url) |>
+          httr2::req_auth_bearer_token(token) |>
+          httr2::req_method("GET") |>
+          httr2::req_perform()
+      }
+      httr2::resp_check_status(resp)
+      httr2::resp_body_raw(resp) |>
+        writeBin(dest)
+
+      R.utils::gunzip(
+        dest,
+        destname = substr(dest, 1, nchar(dest) - 3),
+        remove = FALSE
+      )
     }
+
+    # if (!file.exists(dest)) {
+    #   download.file(url, destfile = dest, mode = "wb")
+    #   # unzipping .gz but keeping them in the folder
+    #   # dest is the compressed file path, destname is the uncompressed file inside (the way splicing is INSANE in R???????)
+    #   R.utils::gunzip(dest, destname = substr(dest, 1, nchar(dest) - 3), remove = FALSE)
+    # } else {
+    #   message(paste0("File", file_name, "already exists. Skipping download."))
+    # }
   }
 }
